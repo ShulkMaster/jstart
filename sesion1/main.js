e18ecd988bc4127c517d993fc7801026898d3eee
ShulkMaster <shulkmaster@gmail.com>
2020-08-01 17:44:33 -0600

    agregando luz rojo parpadeante

--- sesion1/main.js
+++ sesion1/main.js
@@ -1,10 +1,43 @@
 const startButton = document.getElementById('start');
+const rojo = document.getElementById('rojo');
+const verde = document.getElementById('verde');
+const azul = document.getElementById('azul');
+const amarillo = document.getElementById('amarillo');
+
+/**
+ * 1 = Rojo
+ * 2 = Verde
+ * 3 = Amarillo 
+ * 4 = azul
+ */
+
+
 const patron = [];
+let entrada = [];
 
 startButton.addEventListener('click', () => {
     console.log(patron);
     crearPatron(patron);
     console.log(patron);
+});
+
+rojo.addEventListener('click', () => {
+    entrada.push(1);
+});
+
+
+verde.addEventListener('click', () => {
+    entrada.push(2);
+});
+
+
+amarillo.addEventListener('click', () => {
+    entrada.push(3);
+});
+
+
+azul.addEventListener('click', () => {
+    entrada.push(4);
 });
 
 function crearPatron(lista) {
@@ -14,10 +47,11 @@ function crearPatron(lista) {
         const paso = crearPaso();
         lista.push(paso);
     }
+    (patron.length % 2) == 0 ? activar(rojo) : desactivar(rojo);
 }
 
 function crearPaso() {
-    return Math.floor(Math.random() * (3)) + 1;
+    return Math.floor(Math.random() * 4) + 1;
 }
 
 function crearPatronInicial(lista) {
@@ -27,4 +61,12 @@ function crearPatronInicial(lista) {
     lista.push(paso1);
     lista.push(paso2);
     lista.push(paso3);
+}
+
+function activar(elemento) {
+    elemento.classList.add('activo');
+}
+
+function desactivar(elemento) {
+    elemento.classList.remove('activo');
 }
